2192285dac2fa7b8afba19ecace2ec578d349147
Kushagra <kushagra.jaiswal.cer13@itbhu.ac.in>
2022-03-13 14:25:14 +0530

    Add Conversation sample

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.myapplication.data.MockData
+import com.example.myapplication.ui.composables.Conversation
 import com.example.myapplication.ui.composables.MessageCard
 import com.example.myapplication.ui.model.Message
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -12,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                MessageCard(message = Message("Florina", "This is Jetpack compose wohoo!"))
+                Conversation(messages = MockData.conversationSample)
             }
         }
     }
