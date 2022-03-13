183ac112eaa77412c16dd9e084d9ecfe1d971a4e
Kushagra <kushagra.jaiswal.cer13@itbhu.ac.in>
2022-03-13 14:25:32 +0530

    optimise imports

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,8 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.myapplication.data.MockData
 import com.example.myapplication.ui.composables.Conversation
-import com.example.myapplication.ui.composables.MessageCard
-import com.example.myapplication.ui.model.Message
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
