6f02737a844752b653afd6ce937f314c55e18456
Kushagra <kushagra.jaiswal.cer13@itbhu.ac.in>
2022-03-13 14:37:38 +0530

    Added expanded animation with color toggle

--- app/src/main/java/com/example/myapplication/ui/composables/MessageCard.kt
+++ app/src/main/java/com/example/myapplication/ui/composables/MessageCard.kt
@@ -1,16 +1,19 @@
 package com.example.myapplication.ui.composables
 
 import android.content.res.Configuration
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +34,17 @@ fun MessageCard(message: Message) {
         )
 
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        var isExpanded by remember {
+            mutableStateOf(false)
+        }
+
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+        Column(
+            modifier = Modifier.clickable { isExpanded = !isExpanded }
+        ) {
             Text(
                 text = message.author,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -39,10 +52,13 @@ fun MessageCard(message: Message) {
             )
             Spacer(modifier = Modifier.height(8.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium, elevation = 1.dp, color = surfaceColor
+            ) {
                 Text(
                     text = message.body,
                     modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
